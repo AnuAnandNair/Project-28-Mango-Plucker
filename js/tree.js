ec7136a9a46c9bd1fd2c7ae5c3944bd26a4fdb94
AnuAnandNair <69575373+AnuAnandNair@users.noreply.github.com>
2020-10-18 10:37:41 +0530

    Update tree.js

--- js/tree.js
+++ js/tree.js
@@ -11,12 +11,13 @@ class tree{
     }
     display(){
         var pos=this.body.position;
-        push();
-		translate(pos.x, pos.y+10);
-		fill(255);
-        imageMode(CENTER);
-        image(this.image,0,0,3*this.width,2*this.height);//if width and height are same as that of the tree body, then the stone cannot touch the mangoes. 
+        //push();
+	//translate(pos.x, pos.y);
+	fill(255);        
+        //image(this.image,0,0,3*this.width,2*this.height);//if width and height are same as that of the tree body, then the stone cannot touch the mangoes. 
         //Tree body restricts the movement of stone towards mango and stone falls off near the border of the tree
-        pop();
+	imageMode(CENTER);
+	image(this.image,pos.x,pos.y,this.width*3,this.height*2);
+        //pop();
     }
 }
